c82f7e80dc6b1c97c3edff72ffbe2d338843fc25
sadamsanthosh <santhoshsadam22@gmail.com>
2025-08-06 14:39:34 +0530

    Add files via upload

---
+++ arrays/PrintingElements.java
@@ -0,0 +1,18 @@
+package arrays;
+
+import java.util.Arrays;
+import java.util.Scanner;
+
+public class PrintingElements {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		//program to print elements with static values
+
+		int a[]= {1,2,3,4,5};
+		System.out.println(Arrays.toString(a));
+	}
+
+}
+
+
